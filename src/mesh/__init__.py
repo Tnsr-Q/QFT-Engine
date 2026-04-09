d396a5b6948514858074d027764fee76c4fdf8ad
Tanner Jacobsen <quantquiplabs@gmail.com>
2026-04-09 03:22:20 -0500

    Add proto schema, unified mesh, and universality discovery modules

---
+++ src/mesh/__init__.py
@@ -0,0 +1,11 @@
+from .topology import DeviceTopology, DeviceType, JAXMeshAdapter, MeshAxis, PyTorchMeshAdapter
+from .unified_mesh import UnifiedMesh
+
+__all__ = [
+    "DeviceTopology",
+    "DeviceType",
+    "JAXMeshAdapter",
+    "MeshAxis",
+    "PyTorchMeshAdapter",
+    "UnifiedMesh",
+]
---
+++ src/proto/__init__.py
@@ -0,0 +1,12 @@
+from .constraint_schema import AssumptionTag, ConstraintRole, PhysicsPredicate, StatusLevel
+from .registry import PredicateRegistry
+from .serializer import Serializer
+
+__all__ = [
+    "AssumptionTag",
+    "ConstraintRole",
+    "PhysicsPredicate",
+    "StatusLevel",
+    "PredicateRegistry",
+    "Serializer",
+]
